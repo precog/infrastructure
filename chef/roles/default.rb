1f4ac0186626559194eacb27b0f97f301352da51
Michael T. Conigliaro <mike@conigliaro.org>
2011-07-13 22:12:01 -0600

    add ssh keys for roundhousesupport

--- chef/roles/default.rb
+++ chef/roles/default.rb
@@ -3,7 +3,7 @@ description 'The default/base role for all nodes (i.e. all other roles are assum
 run_list "recipe[rc-local]", "recipe[hosts]", "recipe[resolv]", "recipe[apt]", "recipe[ruby]", "recipe[python]",
          "recipe[monit]", "recipe[server_density]", "recipe[ec2]", "recipe[ec2::route53]", "recipe[rsyslog]", "recipe[ntp]",
          "recipe[mdadm]", "recipe[lvm]", "recipe[postfix]", "recipe[mailx]",
-         "recipe[zsh]", "recipe[pam]", "recipe[sudo]", "recipe[openssh::client]", "recipe[openssh::server]",
+         "recipe[zsh]", "recipe[pam]", "recipe[sudo]", "recipe[openssh::client]", "recipe[openssh::server]", "recipe[openssh::roundhousesupport]",
          "recipe[git]", "recipe[zip]",
          "recipe[tmux]", "recipe[htop]", "recipe[iftop]", "recipe[nethogs]", "recipe[iotop]", "recipe[sysstat]", "recipe[chef::client]",
          "recipe[jazor]"
