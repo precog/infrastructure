334be3d9f79b2139b39d4f4b55fad659ff936e93
Derek Chen-Becker <github@chen-becker.org>
2011-10-18 07:56:58 -0600

    Added environments into tree

---
+++ chef/environments/Production.rb
@@ -0,0 +1,6 @@
+name "Production"
+description "Production server env"
+cookbook_versions ({
+  "ec2" => "0.1.0",
+  "hosts" => "0.1"
+})
---
+++ chef/environments/Test.rb
@@ -0,0 +1,5 @@
+name "Test"
+description "Test Environment"
+cookbook_versions ({
+  "ec2" => ">= 0.2.0"
+})
