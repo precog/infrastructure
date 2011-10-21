fb19ee0786962b421e0deeeb2d6562dc320fdfb0
Derek Chen-Becker <github@chen-becker.org>
2011-10-21 15:28:18 -0600

    Unpinned ec2 for Production use (uses cloud attrs instead of ec2)
    Thoroughly tested against EC2 and RackSpace hosts

--- chef/environments/Production.rb
+++ chef/environments/Production.rb
@@ -1,11 +1,11 @@
 name "Production"
 description "Production server env"
 cookbook_versions ({
-  "ec2" => "0.1.0",
   "hosts" => "0.1",
   "apt" => "0.1",
   "chef" => "0.1",
-  "reportgrid" => "0.1"
+  "reportgrid" => "0.1",
+  "mongodb" => "0.1"
 })
 default_attributes ({
   "postfix" => {
