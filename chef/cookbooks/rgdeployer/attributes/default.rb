9d8c87c370d7a98cec5da1c9faccabf1dcb63768
Derek Chen-Becker <github@chen-becker.org>
2011-11-11 23:57:46 -0700

    Added default deployer agent options (had forgotten)

--- chef/cookbooks/rgdeployer/attributes/default.rb
+++ chef/cookbooks/rgdeployer/attributes/default.rb
@@ -4,3 +4,5 @@ default[:deployer][:key] = "d88fje9irjspskrewrf9r"
 default[:deployer][:only_stable] = "true"
 
 default[:deployer][:mongo] = ["localhost:27017"]
+
+default[:deployer][:agent_options] = []
