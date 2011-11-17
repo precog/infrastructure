3559c740459c92a8a359787599dfedd97a3eba38
Derek Chen-Becker <github@chen-becker.org>
2011-11-17 11:24:47 -0700

    Updates to monitor billing for nagios

--- chef/cookbooks/nagios/files/default/plugins/check_rg_service.rb
+++ chef/cookbooks/nagios/files/default/plugins/check_rg_service.rb
@@ -21,9 +21,10 @@ end
 def safe_get (d, *keys)
   begin
     s = d
-    keys.each {|k| s = s[k]}
-    yield s
+    keys.each {|k|s = s[k] }
+    yield [s]
   rescue NoMethodError => e
+    yield []
   end
 end
 
@@ -35,7 +36,7 @@ begin
   Net::HTTP.start(url.host, url.port) do |http|
     http.request_get(path) do |response|
       if not response.is_a? Net::HTTPOK then
-        $stderr.put("Unknown:Error collecting stats: #{response.read_body}")
+        $stderr.put("TIMING UNKNOWN:Error collecting stats: #{response.read_body}")
       else
         begin
           data = JSON.parse(response.read_body)
@@ -45,42 +46,53 @@ begin
             error_value = nil
 
             result = ["GET", "POST"].map do |verb|
-              safe_get(data,"requests",verb,"errors","errorDistribution") {|errors|
-                error_value = errors.map{|k,v| v["5min x 1"].to_i }.flatten.reduce(:+)
+              safe_get(data,"requests",verb,"errors","errorDistribution") { |errors|
+                error_value = errors.map{|obj| obj.map{|k,v| v["5min x 1"].to_i }}.flatten.reduce(:+)
               }
               error_value ||= 0
               error_value
             end.reduce(:+)
 
             if result > critical.to_i then
-              puts "Critical:Total of #{error_value} errors on service"
+              puts "TIMING CRITICAL:Total of #{error_value} errors on service"
             elsif result > warn.to_i then
-              puts "Warning:Total of #{error_value} errors on service"
+              puts "TIMING WARNING:Total of #{error_value} errors on service"
             else
               puts "OK:Total of #{error_value} errors on service"
             end
           when "response"
             times = ["GET", "POST"].map do |verb|
-              safe_get(data,"requests",verb,"timing","averageTime","5min x 1") {|v| [verb,v[0].to_f] }
+              puts "Checking #{verb}"
+              result = safe_get(data,"requests",verb,"timing","averageTime","5min x 1",0) {|d| d.flatten }
+              puts "Result for #{verb} = #{result}"
+              [verb,if result.length == 1 then result[0] else nil end]
+            end.select {|r| r[1] != nil }
+
+            if times.length == 2 then
+              puts "Both vals"
+              max = times.sort{|a,b| a[1] <=> b[1]}[-1]
+            elsif times.length == 1 then
+              puts "Single val: #{times}"
+              max = times[0]
             end
 
-            max = times.sort{|a,b| a[1] <=> b[1]}[-1]
+            puts "Max = #{max[0]}, #{max[1]}"
 
             if max[1] > critical.to_f then
-              puts "Critical:#{max[0]} response average is #{max[1]}"
+              puts "TIMING CRITICAL:#{max[0]} response average is #{max[1]}"
             elsif max[1] > warn.to_f then
-              puts "Warning:#{max[0]} response average is #{max[1]}"
+              puts "TIMING WARNING:#{max[0]} response average is #{max[1]}"
             else
               puts "OK:#{max[0]} response average is #{max[1]}"
             end
           end
         rescue => e
-          $stderr.puts("Unknown:Error parsing response: #{e.message}")
+          $stderr.puts("TIMING UNKNOWN:Error parsing response: #{e.message}")
         end
       end
     end
   end
 rescue => e
-  $stderr.puts("Unknown:Error processing poll: #{e.message}")
+  $stderr.puts("TIMING UNKNOWN:Error processing poll: #{e.message}")
   exit(3)
 end
