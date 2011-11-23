1130e3476828fd47f745cb9a61a844aa9e6151f6
Derek Chen-Becker <derek@reportgrid.com>
2011-11-23 07:45:52 -0700

    Started on JMeter remote agent support

---
+++ chef/cookbooks/jmeter/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: jmeter
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "java"
+
+package "jmeter-http"
