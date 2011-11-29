caf8764674931e6b2596f08c4756336fcd327bde
Derek Chen-Becker <derek@reportgrid.com>
2011-11-29 12:14:25 -0700

    Sync app server logs to S3 for archiving

---
+++ chef/cookbooks/reportgrid/recipes/appserver_log_backup.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: reportgrid
+# Recipe:: appserver_log_backup
+#
+# Copyright 2011, ReportGrid
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cron "sync_app_logs_to_s3" do
+  command "s3cmd sync --no-delete-removed --no-progress --exclude='*' --include='analytics-v1-2*' /var/log/reportgrid/ s3://ops.reportgrid.com/logs/`hostname`/"
+  hour "1"
+end
