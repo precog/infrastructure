512840a70ee3e7ce3677e17d2918ba8e59721ce4
Derek Chen-Becker <derek@reportgrid.com>
2011-12-16 11:32:57 -0700

    Added staging environment for deployment testing against prod

--- chef/cookbooks/rgdeployer/attributes/default.rb
+++ chef/cookbooks/rgdeployer/attributes/default.rb
@@ -3,6 +3,7 @@ default[:deployer][:server] = "http://devops01.reportgrid.com:29999/"
 default[:deployer][:key] = "d88fje9irjspskrewrf9r"
 default[:deployer][:only_stable] = "true"
 
+default[:deployer][:mongodb] = "deployment"
 default[:deployer][:mongo] = ["localhost:27017"]
 
 default[:deployer][:agent_options] = []
---
+++ chef/environments/Staging.rb
@@ -0,0 +1,20 @@
+name "Staging"
+description "Staging server env"
+cookbook_versions ({
+})
+default_attributes ({
+  "postfix" => {
+    "root_email" => "operations@reportgrid.com"
+  },
+  "apache" => {
+    "listen_ports" => [ "20000" ]
+  }
+})
+override_attributes ({
+  "deployer" => {
+    "notified" => "operations@reportgrid.com",
+    "server" => "http://stageapp01.reportgrid.com:29999/",
+    "key" => "justanotherstagingkey",
+    "mongodb" => "deployment-staging"
+  }
+})
