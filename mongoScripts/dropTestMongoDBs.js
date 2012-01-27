c2d40e385ead1c53f223e227d0fdc2fe368ae878
Derek Chen-Becker <derek@reportgrid.com>
2012-01-26 17:03:25 -0700

    Tweak to dropTest script

--- mongoScripts/dropTestMongoDBs.js
+++ mongoScripts/dropTestMongoDBs.js
@@ -1,7 +1,8 @@
 var dbs = db.getMongo().getDBNames()
 
 for (var i in dbs) {
-    if (dbs[i].match("test")) {
+    if (dbs[i].match(/test.*/)) {
+        print("Dropping " + dbs[i]);
 	db.getSisterDB(dbs[i]).dropDatabase();
     }
 }
