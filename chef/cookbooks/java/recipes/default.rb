dbc86461a7aa5577d5e80b67f64a590cbae39c63
Derek Chen-Becker <derek@reportgrid.com>
2012-03-20 20:48:02 -0600

    Fix Java cookbook to use new script for downloading package
    Canonical removed Java from their repos, so we have to work around

--- chef/cookbooks/java/recipes/default.rb
+++ chef/cookbooks/java/recipes/default.rb
@@ -2,10 +2,22 @@
 # Cookbook Name:: java
 # Recipe:: default
 #
-# Copyright 2010, ReportGrid
+# Copyright 2010-2012, ReportGrid
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Need to manually grab Java now that Canonical doesn't carry it
+execute "manually package Java" do
+command """
+cd ~/
+wget https://raw.github.com/flexiondotorg/oab-java6/master/oab-java6.sh -O oab-java6.sh
+chmod +x oab-java6.sh
+sudo ./oab-java6.sh
+"""
+  not_if "test -f ~/oab-java6.sh"
+end
+
 execute "update-java-alternatives" do
   command "/usr/sbin/update-java-alternatives --set java-6-sun"
   ignore_failure true
