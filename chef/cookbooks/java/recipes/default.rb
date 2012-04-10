6cdb8cd93fb32e83718c7cd4075fb7e4f33fb8ac
Derek Chen-Becker <derek@reportgrid.com>
2012-04-10 10:24:35 -0600

    Bump java to install openjdk6 (and purge sun-java6)

--- chef/cookbooks/java/recipes/default.rb
+++ chef/cookbooks/java/recipes/default.rb
@@ -8,32 +8,54 @@
 #
 
 # Need to manually grab Java now that Canonical doesn't carry it
-execute "manually package Java" do
-command """
-cd ~/
-wget https://raw.github.com/flexiondotorg/oab-java6/master/oab-java6.sh -O oab-java6.sh
-chmod +x oab-java6.sh
-sudo ./oab-java6.sh
-"""
-  not_if "test -f ~/oab-java6.sh"
-end
+#execute "manually package/update Java" do
+#command """
+#cd ~/
+#wget https://raw.github.com/dchenbecker/oab-java6/master/oab-java6.sh -O oab-java6.sh
+#chmod +x oab-java6.sh
+#sudo ./oab-java6.sh -s
+#"""
+#end
+#
+#execute "update-java-alternatives" do
+#  command "/usr/sbin/update-java-alternatives --set java-6-sun"
+#  ignore_failure true
+#  action :nothing
+#end
+#
+#package "sun-java6-jdk" do
+#  package_name "sun-java6-jdk"
+#  response_file "java.seed"
+#  options '-o Dpkg::Options::="--force-confold"'
+#  notifies :run, resources(:execute => "update-java-alternatives"), :immediately
+#  ignore_failure true
+#end
+#
+#cookbook_file "java.security" do
+#  source "java.security"
+#  path "/etc/java-6-sun/security/java.security"
+#  mode "644"
+#end
 
-execute "update-java-alternatives" do
-  command "/usr/sbin/update-java-alternatives --set java-6-sun"
-  ignore_failure true
-  action :nothing
+#package "java-common"
+#
+#["sun-java6-bin_6.31-2~lucid1_amd64.deb", "sun-java6-fonts_6.31-2~lucid1_all.deb", "sun-java6-jdk_6.31-2~lucid1_amd64.deb", "sun-java6-jre_6.31-2~lucid1_all.deb"].each { |filename|
+#  cookbook_file "/tmp/#{filename}"
+#}
+#
+#execute "install_java" do
+#  not_if "dpkg -l 'sun-java6*' | grep '^ii'"
+#  command "dpkg -i /tmp/sun-java6*"
+#  ignore_failure true
+#end
+
+# Use openjdk to keep things simple
+package "default-jdk"
+
+package "sun-java6-jre" do
+  action :purge
 end
 
 package "sun-java6-jdk" do
-  package_name "sun-java6-jdk"
-  response_file "java.seed"
-  options '-o Dpkg::Options::="--force-confold"'
-  notifies :run, resources(:execute => "update-java-alternatives"), :immediately
-  ignore_failure true
-end
-
-cookbook_file "java.security" do
-  source "java.security"
-  path "/etc/java-6-sun/security/java.security"
-  mode "644"
+  action :purge
 end
