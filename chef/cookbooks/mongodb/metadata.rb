198c2900b8a4974d1f3ceecc736f0f0f52f567f3
Derek Chen-Becker <derek@reportgrid.com>
2012-05-01 08:58:41 -0600

    Disable package install in mongo recipe to allow for nightlies

--- chef/cookbooks/mongodb/metadata.rb
+++ chef/cookbooks/mongodb/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.10"
+version          "0.3"
 depends          "ec2"
 depends          "apt"
