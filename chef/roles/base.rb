5f3c79a5fb0f8228061ede04a93b457ef622c42b
Derek Chen-Becker <derek@reportgrid.com>
2012-05-17 22:40:31 -0600

    Switch to per-used SSH keys. Modified SSHD to log key fingerprint (auditing)

--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -3,12 +3,12 @@ description 'The default/base role for all nodes (i.e. all other roles are assum
 #run_list "recipe[resolv]",
 #         "recipe[monit]", "recipe[server_density]", "recipe[ec2]", "recipe[rsyslog]", 
 #         "recipe[mdadm]", "recipe[lvm]",
-#         "recipe[zsh]", "recipe[pam]", "recipe[openssh::client]", "recipe[openssh::server]",
-#         "recipe[git]", "recipe[zip]",
-#         "recipe[tmux]", "recipe[iftop]", "recipe[nethogs]", "recipe[sysstat]",
 #         "recipe[jazor]"
 
 run_list "recipe[ubuntu_user]", "recipe[sudo]", "recipe[rc-local]", "recipe[hosts]", "recipe[at]", "recipe[ruby]",
+         "recipe[git]", "recipe[zip]", "recipe[monit]",
+         "recipe[zsh]", "recipe[pam]", "recipe[openssh::client]", "recipe[openssh::server]",
+         "recipe[tmux]", "recipe[iftop]", "recipe[nethogs]", "recipe[sysstat]",
          "recipe[python]", "recipe[monit]", "recipe[ec2::route53]", "recipe[postfix]", "recipe[rsyslog]",
          "recipe[mailx]", "recipe[munin::client]", "recipe[apt]", "recipe[chef::client]", "recipe[ntp]",
          "recipe[htop]", "recipe[iotop]", "recipe[sysstat]", "recipe[pam]"
