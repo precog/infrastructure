d6937048f3c9a7eed1e61cb70557aab0b42fad93
Derek Chen-Becker <derek@reportgrid.com>
2012-06-15 13:59:16 -0600

    Add Dev V2 env/config for deployment of V2 services

---
+++ chef/environments/DevV2.rb
@@ -0,0 +1,30 @@
+name "DevV2"
+description "V2 Dev Environment"
+cookbook_versions ({
+})
+default_attributes ({
+  "postfix" => {
+    "root_email" => "operations@reportgrid.com"
+  },
+  "apache" => {
+    "listen_ports" => [ "20000" ]
+  }
+})
+override_attributes ({
+  "reportgrid" => {
+    "tokens" => {
+      "test" => {
+        "rollup" => "5"
+      }
+    },
+    "visualization" => {
+      "s3url" => "s3://ops.reportgrid.com/viz-dev/"
+    }
+  },
+  "deployer" => {
+    "mongodb" => "deployment-qclusdev",
+    "notified" => "operations@reportgrid.com",
+    "server" => "http://devqclus02.reportgrid.com:29999/",
+    "key" => "aV2devdeployIthink"
+  }
+})
