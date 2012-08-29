a26b35d55cd216dc358c7e92d1d9810415ec2402
Derek Chen-Becker <derek@precog.com>
2012-08-29 10:20:18 -0600

    Bump FS thresholds to 85%

--- chef/cookbooks/mongodb/metadata.rb
+++ chef/cookbooks/mongodb/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.1"
+version          "0.3.2"
 depends          "ec2"
 depends          "apt"
