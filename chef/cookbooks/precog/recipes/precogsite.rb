8dcde422f5ba7f256daded4debaa472dbe9ae431
Derek Chen-Becker <derek@precog.com>
2012-09-27 22:26:03 -0600

    Changes for migration of CMS site to new servers

--- chef/cookbooks/precog/recipes/precogsite.rb
+++ chef/cookbooks/precog/recipes/precogsite.rb
@@ -10,16 +10,16 @@ include_recipe "apache2"
 # These enable the relevant modules
 include_recipe "apache2::mod_php5"
 
-directory "/var/www/precogsite" do
+directory "/var/www/cmssite" do
   recursive true
   owner "ubuntu"
   group "www-data"
-  mode  "755"
+  mode  "2775"
 end
 
 web_app "precogsite" do
   server_name "www.precog.com"
-  server_aliases ["precog.com","site.precog.com"]
+  server_aliases ["precog.com","site.precog.com","reportgrid.com","www.reportgrid.com","web1.precog.com","web2.precog.com"]
   template "precogsite.conf.erb"
-  docroot "/var/www/precogsite"
+  docroot "/var/www/cmssite"
 end
