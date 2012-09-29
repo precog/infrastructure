641f7d03f3593f885ca899ac9185f56bdf1947f2
Derek Chen-Becker <derek@precog.com>
2012-09-29 08:02:08 -0600

    Fix rsyslog recipe for Ubuntu 12.04
    The $KLogPath param in 12.04 makes rsyslog spin at 200% CPU
    https://bugs.launchpad.net/ubuntu/+source/rsyslog/+bug/401433

--- chef/cookbooks/rsyslog/metadata.rb
+++ chef/cookbooks/rsyslog/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures rsyslog"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.1.1"
