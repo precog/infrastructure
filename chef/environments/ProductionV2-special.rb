97a56ff3876213504fd9b4a0fb26d6c363348ba4
Derek Chen-Becker <derek@precog.com>
2012-10-01 09:35:30 -0600

    Add env for special server

---
+++ chef/environments/ProductionV2-special.rb
@@ -0,0 +1,36 @@
+name "ProductionV2-Special"
+description "ProductionV2 special server env"
+cookbook_versions ({
+})
+default_attributes ({
+  "postfix" => {
+    "root_email" => "operations@reportgrid.com"
+  },
+  "apache" => {
+    "listen_ports" => [ "20000" ]
+  }
+})
+override_attributes ({
+  "reportgrid" => {
+    "tokens" => {
+      "test" => {
+        "rollup" => "5"
+      }
+    },
+    "visualization" => {
+      "s3url" => "s3://ops.reportgrid.com/viz-prodv2-special/"
+    }
+  },
+  "precog" => {
+    "ide" => {
+      "s3url" => "s3://ops.reportgrid.com/ide-prodv2-special/"
+    }
+  },
+  "deployer" => {
+    "mongodb" => "deployment-prodv2-special",
+    "notified" => "operations@reportgrid.com",
+    "server" => "http://nebula.precog.com:29999/",
+    "key" => "aV2prodspecdeployIthink"
+  }
+})
+
