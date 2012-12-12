0bbfa8062a0a5a3cc184bf05bf36575dc43116f3
Derek Chen-Becker <derek@precog.com>
2012-12-12 13:29:16 -0700

    Add support for add-apt-repository to chef recipes via LWRP

--- chef/cookbooks/apt/metadata.rb
+++ chef/cookbooks/apt/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures apt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2"
+version          "0.2.1"
