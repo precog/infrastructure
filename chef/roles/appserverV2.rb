d7ea3c2667f39fe34c9448b5d5d163cb564dd55b
Derek Chen-Becker <derek@precog.com>
2012-12-14 11:03:06 -0700

    Add nginx to all app server

--- chef/roles/appserverV2.rb
+++ chef/roles/appserverV2.rb
@@ -1,3 +1,3 @@
 name "appserverV2"
 description 'ReportGrid application server role v2'
-run_list "recipe[java]", "recipe[mongodb::mongos]", "recipe[reportgrid::ssl_proxy]", "recipe[haproxy]", "recipe[rgdeployer]", "recipe[s3tools]", "recipe[reportgrid::appserver_log_backup]", "recipe[reportgrid::utils]","recipe[precog::precog-io-redirect]"
+run_list "recipe[java]", "recipe[mongodb::mongos]", "recipe[reportgrid::ssl_proxy]", "recipe[haproxy]", "recipe[rgdeployer]", "recipe[s3tools]", "recipe[reportgrid::appserver_log_backup]", "recipe[reportgrid::utils]","recipe[precog::precog-io-redirect]", "recipe[nginx]"
