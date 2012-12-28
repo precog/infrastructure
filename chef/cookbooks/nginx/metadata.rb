e95bfc6cb508f543bdea5c166b61e3949243d9e2
Derek Chen-Becker <derek@precog.com>
2012-12-28 09:35:27 -0700

    Update nginx conf to use cloudfront

--- chef/cookbooks/nginx/metadata.rb
+++ chef/cookbooks/nginx/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.4"
+version          "0.1.5"
 depends          "apt"
