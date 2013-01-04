834c8e51c8622bca20f74ba11b309857103b6227
Derek Chen-Becker <derek@precog.com>
2013-01-03 17:06:42 -0700

    Added Daniel Sobral to SSH keys

--- chef/cookbooks/ubuntu_user/metadata.rb
+++ chef/cookbooks/ubuntu_user/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures ubuntu_user"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.13"
+version          "0.0.14"
