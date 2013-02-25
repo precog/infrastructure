9cf9873b9df986f6adb32d36dfe70f5403d651fc
Daniel C. Sobral <dcsobral@precog.com>
2013-02-25 03:50:27 -0500

    Enable stats on haproxy
    Enable stats on haproxy on a separate port, protected by password.

--- chef/cookbooks/haproxyV2/metadata.rb
+++ chef/cookbooks/haproxyV2/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures haproxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.18"
+version          "0.1.19"
