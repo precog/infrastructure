0f54c82eb0f42f8b2eb43aba290f1e8a512d595f
Daniel C. Sobral <dcsobral@precog.com>
2013-03-28 22:36:01 -0400

    Disable all varnish trickery
    Due to an issue with new user creation from an anonymous session,
    all varnish trickery is temporarily disabled.

--- chef/roles/precogwebsite.rb
+++ chef/roles/precogwebsite.rb
@@ -4,4 +4,5 @@ run_list "recipe[apache2]"
 override_attributes "apache" => { "listen_ports" => [ "80", "443" ] }, "extra_hosts" => [ 
   "10.38.35.132\twebdb.precog.com web1-internal.precog.com",
   "10.38.35.133\tweb2-internal.precog.com"
+  "10.38.35.134\tweb4-internal.precog.com"
 ]
--- chef/roles/precogwebsitevarnish.rb
+++ chef/roles/precogwebsitevarnish.rb
@@ -5,4 +5,5 @@ run_list "recipe[varnish]"
 override_attributes "apache" => { "listen_ports" => [ "8080", "443" ] }, "extra_hosts" => [ 
   "10.38.35.132\twebdb.precog.com web1-internal.precog.com",
   "10.38.35.133\tweb2-internal.precog.com"
+  "10.38.35.134\tweb4-internal.precog.com"
 ]
