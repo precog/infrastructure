cc19c7f85eb868ea86d2fbc8e768de7851710435
Daniel C. Sobral <dcsobral@precog.com>
2013-04-05 16:42:34 -0400

    Add pagerduty integration to nagios and devops

--- chef/roles/monitoring.rb
+++ chef/roles/monitoring.rb
@@ -1,11 +1,13 @@
 name "monitoring"
 description "Monitoring server"
 run_list(
-  "recipe[pymongo]", "recipe[nagios::server]", "recipe[munin::server]", "recipe[munin::client]"
+  "recipe[pymongo]", "recipe[nagios::server]", "recipe[munin::server]", "recipe[munin::client]",
+  "recipe[nagios::pagerduty]"
 )
 
 default_attributes(
   "nagios" => {
-    "server_auth_method" => "htauth"
+    "server_auth_method" => "htauth",
+    'pagerduty_key' => '9966a8ce29fc472bb0c46682dcb61752'
   }
 )
