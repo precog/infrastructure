b6820f6e1ea24ca8f327f2f0f7bf87814271123e
Daniel C. Sobral <dcsobral@precog.com>
2013-04-17 02:59:43 -0400

    Add jenkins key to ubuntu autorized

--- chef/cookbooks/ubuntu_user/metadata.rb
+++ chef/cookbooks/ubuntu_user/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "operations@reportgrid.com"
 license          "All rights reserved"
 description      "Installs/Configures ubuntu_user"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.14"
+version          "0.0.15"
