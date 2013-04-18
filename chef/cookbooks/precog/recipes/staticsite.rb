93af0075200223bf2c81d9f7fb6026c7b6855f2c
Daniel C. Sobral <dcsobral@precog.com>
2013-04-17 21:29:49 -0400

    Get static site setup closer to working

--- chef/cookbooks/precog/recipes/staticsite.rb
+++ chef/cookbooks/precog/recipes/staticsite.rb
@@ -10,10 +10,22 @@ include_recipe "precog::nginxcerts"
 include_recipe "nginx::repo"
 include_recipe "nginx"
 
-directory "/var/www/staticsite" do
+directory "/var/www/precogsite" do
   recursive true
   owner "ubuntu"
   group "www-data"
   mode  "2775"
+end
+
+template "#{node['nginx']['dir']}/sites-available/staticsite" do
+  source "staticsite.erb"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :reload, 'service[nginx]'
+end
+
+nginx_site 'staticsite' do
+  enable true
 end
 
