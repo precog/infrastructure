d89d45b099a17ef319cb54cded4802eb87242a97
Daniel C. Sobral <dcsobral@precog.com>
2013-04-26 20:47:29 -0400

    Update V2 role to deal with insidious SSL on Apache

--- chef/roles/quirrel-shardV2.rb
+++ chef/roles/quirrel-shardV2.rb
@@ -1,3 +1,4 @@
 name "quirrel-shardV2"
 description "Quirrel shard member"
 run_list "recipe[rgdeployer]", "recipe[precog::utils]", "recipe[java]", "recipe[haproxyV2]", "recipe[s3tools]", "recipe[precog::ide]", "recipe[precog::shardtmp]", "recipe[reportgrid::builder]"
+override_attributes "apache" => { "listen_ports" => [ "20000" ] }
