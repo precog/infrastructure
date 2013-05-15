3f9d5b478ec69209a44fe596f5ea4a604edaa43d
Derek Chen-Becker <derek@precog.com>
2013-05-14 18:24:59 -0600

    Trim the fat in kafka local log retention
    It should all be in central anyway, so we don't need to keep the full log

--- chef/cookbooks/kafka/metadata.rb
+++ chef/cookbooks/kafka/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures kafka"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 depends          "java"
 depends          "precog"
