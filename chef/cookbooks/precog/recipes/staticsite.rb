c81cc5b553eacddcc2ee61ff25d1b17b61f01ac6
Daniel C. Sobral <dcsobral@precog.com>
2013-05-30 09:38:20 -0400

    Fix recipe order and remove major duplication on static site

--- chef/cookbooks/precog/recipes/staticsite.rb
+++ chef/cookbooks/precog/recipes/staticsite.rb
@@ -6,10 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "precog::nginxcerts"
 include_recipe "nginx::repo"
 include_recipe "nginx"
 include_recipe "nginx::rrd"
+include_recipe "precog::nginxcerts"
 
 directory '/var/www/precogsite' do
   owner "ubuntu"
@@ -41,49 +41,6 @@ end
     group "www-data"
     mode  "2775"
   end
-end
-
-directory '/etc/nginx/redirects.d' do
-  owner 'root'
-  group 'root'
-  mode 0755
-end
-
-link '/etc/nginx/redirects.d/staticsite.conf' do
-  to '/var/www/precogsite/current/redirects.conf'
-end
-
-template "#{node['nginx']['dir']}/sites-available/staticsite" do
-  source "staticsite.erb"
-  owner "root"
-  group "root"
-  mode 00644
-  notifies :reload, 'service[nginx]'
-end
-
-nginx_site 'staticsite' do
-  enable true
-end
-
-template "#{node['nginx']['dir']}/sites-available/staticsite-ssl" do
-  source "staticsitessl.erb"
-  owner "root"
-  group "root"
-  mode 00644
-  notifies :reload, 'service[nginx]'
-end
-
-nginx_site 'staticsite-ssl' do
-  enable true
-end
-
-munin_plugin "nginx_combined_" do
-  plugin "nginx_combined_127.0.0.1"
-  create_file true
-end
-
-munin_plugin "nginx_memory" do
-  create_file true
 end
 
 directory '/etc/nginx/redirects.d' do
